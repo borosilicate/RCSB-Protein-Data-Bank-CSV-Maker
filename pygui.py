ee6374df5d9cf7dec749934dbf58ea2fec4cdd7c
michael hearn <mjh55154@uga.edu>
2019-02-10 09:21:36 -0500

    Working on the csv part\!...

--- pygui.py
+++ pygui.py
@@ -6,6 +6,7 @@ import re
 app=gui()
 app.addLabel("title","RCSB-Protein-Data-Bank-CSV-Maker")
 app.setLabelBg("title", "Green")
+app.setFont(12)
 searchTotal="Search Total is "
 thing="0"
 app.addLabel("l1",searchTotal+thing)
@@ -13,6 +14,7 @@ app.addLabelEntry("Search word")
 app.setLabelBg("l1", "Green")
 app.addNumericEntry("csvnumb")
 app.setEntryDefault("csvnumb", "Set Number of CSV objects")
+app.addLabelEntry("File-name")
 app.setFont(20) #app.addLabelSecretEntry("")
 csvoarray=["download-url"]
 options=["download-url", "view-url","structure-url", "Method-X-ray-Diffraction", "Resolution-1.5A","Residue-Count","Macromocule","Ligands","Search-Match-Score","Citation-title","pdbx-description","struct-title","key-words",""]
@@ -25,46 +27,61 @@ def press(button):
         one=0
         while one < csvnumb and one < len(options) :
             try:
+                options[0],options[one]=options[one],options[0]
                 app.addLabelOptionBox(csvitem+str(one),options)
+                options[0],options[one]=options[0],options[one]
             except:
                 print("Already have a "+csvitem+str(one))
             one=one+1
-    elif button == "Set-CSV-options":
+    elif button == "Write-File":
         var=True
         numbe=0
         while var:
             try:
-                app.getEntry("csvitem"+str(numbe))
-            
-    elif button == "Search"
+                csvoarray[numbe]=app.getEntry("csvitem"+str(numbe))
+            except:
+                print("All items added up to ")
+            numbe=numbe+1
+        
+    elif button == "Search":
         word = app.getEntry("Search word")
         print("Search word:"+word) #, "Pass:", pwd)
         strg=numm(word)
         print(str(strg))
-app.addButtons(["Search","Set-CSV-#","Set-CSV-options", "Exit"], press)
+    elif button == "Help":
+        helpMessage="This is a program to create a csv file from the rCSB database.\n"
+        helpMessage+="Search will return the quantity of lines to write.\n"
+        helpMessage+="Write-File will write to the name set in the blank or default.csv if blank.\n"
+        helpMessage+="Write-File will take multiple arguments in a blank and seperate them by comma"
+        helpMessage+="Set-CSV-# will add blanks to the csv file unfortunaly they can not be removed.\n"
+        helpMessage+="Exit and Help are self explanatory..."
+        app.infoBox("RCSB-Protein-Data-Bank-CSV-Maker",helpMessage , parent=None)
+app.addButtons(["Search","Write-File","Set-CSV-#","Exit","Help"], press)
 app.addLabelOptionBox(csvitem+"0",options)
-def numm(querry):
+def numm(querry,write=False):
     html = urllib.request.urlopen("http://www.rcsb.org/pdb/search/navbarsearch.do?f=&q="+querry).read()
     qrid=None
     thing="Still Not working"
     if html is not None:
         soup = BeautifulSoup(html, 'lxml')
         for link in soup.find_all('a'):
-            print(link.get('href'))
+            #print(link.get('href'))
             if("qrid=" in str(link.get('href')) ):
-                qrid=re.sub(r'\w+qrid=', '', str(link.get('href')))
+                #print(link.get('href'))
+                qrid=re.sub(r'.*qrid=', '', str(link.get('href'))).strip()
+                print("qrid="+qrid)
                 #qrid=link.get('href').strip('http://www.rcsb.org/pdb/results/results.do?tabtoshow=Current&qrid=')
     if qrid is not None:
-        print("qrid==",qrid)
+        print("in not none qrid==",qrid)
         #http://www.rcsb.org/pdb/results/results.do?tabtoshow=Current&qrid=4BEA411F
         html2 = urllib.request.urlopen("http://www.rcsb.org/pdb/results/results.do?tabtoshow=Current&qrid="+qrid).read()
         soup = BeautifulSoup(html2, 'lxml')
         #<span id="getResultSizeValue">28590</span>
         thing=soup.find_all(id="getResultSizeValue")
-        print(str(thing))
-        app.addLabel("l1",searchTotal+str(thing))
-        for tag in soup.find_all(thing):
-            print(str(tag))
+        print("soup.find_all(resultsize id)",str(thing))
+        app.setLabel("l1",searchTotal+str(thing))
+        #for tag in soup.find_all(thing):
+            #print(str(tag))
         print(str(thing)+" YES WORKING!!!")
     else:
         print("ERROR No qrid= value found ERROR")
